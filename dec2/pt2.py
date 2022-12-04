1678ddbbd909b2a73ce3b5a3b95336734d8bfe80
Anna Rylander <annaelida.rylander@gmail.com>
2022-12-04 20:58:24 +0100

    refactor/opt/homebrew/bin/python3 /Users/annarylander/Documents/Projects/advent-of-code/advent-of-code-2022/dec2/pt2.py

--- dec2/pt2.py
+++ dec2/pt2.py
@@ -2,63 +2,64 @@ import os
 import sys
 
 os.chdir(os.path.dirname(sys.argv[0]))
-with open("input.txt", "r") as input_file:
-    data = input_file.read().split("\n")
 
-game_score = {"X": 0,
-"Y": 3,
-"Z": 6}
 
-hand_score = {"A": 1,
-"B": 2,
-"C": 3}
+def load_data():
+    with open("input.txt", "r") as input_file:
+        data = input_file.read().split("\n")
+    return data
+
+X = "X"
+Y = "Y"
+Z = "Z"
+
+A = "A"
+B = "B"
+C = "C"
+
+game_score = {X: 0, Y: 3, Z: 6}
+hand_score = {A: 1, B: 2, C: 3}
 
 # get my hand
 def get_hand(result, opponent):
-    hand = ""
-    if result == "Y":
+    if result == Y:
         hand = opponent
-    elif result == "Z":
-        if opponent == "A":
-            hand = "B"
-        elif opponent == "B":
-            hand = "C"
-        elif opponent == "C":
-            hand = "A"
-    elif result == "X":
-        if opponent == "A":
-            hand = "C"
-        elif opponent == "B":
-            hand = "A"
-        elif opponent == "C":
-            hand = "B"
-    print("hand:", hand)
+    elif result == Z:
+        if opponent == A:
+            hand = B
+        elif opponent == B:
+            hand = C
+        elif opponent == C:
+            hand = A
+    elif result == X:
+        if opponent == A:
+            hand = C
+        elif opponent == B:
+            hand = A
+        elif opponent == C:
+            hand = B
     return hand
-
 
 # get my score
 def get_hand_score(hand):
     score = hand_score[hand]
     return score
 
+if __name__ == "__main__":
+    # Loop through each round and calculate total score
+    total_score = 0
+    data = load_data()
+    for line in data:
+        opponent = line[0]
+        result = line[2]
+        print("result:", result)
+        print("opponent:", opponent)
 
-# Loop through each round and calculate total score
-total_score = 0
-for i in range(len(data)):
-    result = data[i][2]
-    opponent = data[i][0]
-    print("result:", result)
-    print("opponent:", opponent)
+        # get score for win/loss/draw
+        total_score += game_score[result]
 
-    #get score for win/loss/draw
-    total_score += game_score[result]
+        # get score for hand
+        hand = get_hand(result, opponent)
+        total_score += get_hand_score(hand)
 
-    #get score for hand
-    hand = get_hand(result, opponent)
-    score = get_hand_score(hand)
-    total_score += score
-
-
-
-print("total score:", total_score)
-
+    print("total_score:", total_score)
