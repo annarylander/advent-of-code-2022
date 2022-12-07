16ba07121b269107714abbf4caa6593299dc128a
Anna Rylander <annaelida.rylander@gmail.com>
2022-12-07 08:07:07 +0100

    pt1 and 2

--- dec3/pt1.py
+++ dec3/pt1.py
@@ -3,8 +3,6 @@ def load_data():
         data = input_file.read().split("\n")
     return data
 
-data = load_data()
-
 
 # split string into two parts
 def split_string(line):
@@ -14,27 +12,30 @@ def split_string(line):
 
 # get the character that appears in both parts
 def get_char(firstpart, secondpart):
+    secondpart_set = set(secondpart)
     for char in firstpart:
-        if char in secondpart:
+        if char in secondpart_set:
             return char
     return None
 
 
-# calculate priority
+# convert character to number
 def calculate_priority(char):
     if char.isupper():
         return ord(char) - 38
     else:
         return ord(char) - 96
 
-# sum up the priorities
-sum_priority = 0
 
-for line in data:
-    compartment_1, compartment_2 = split_string(line)
-    char = get_char(compartment_1, compartment_2)
-    prio = calculate_priority(char)
-    sum_priority += prio
+if __name__ == "__main__":
+    # sum up the priority-numbers
+    sum_priority = 0
 
-print("sum_priority:", sum_priority)
+    data = load_data()
+    for line in data:
+        compartment_1, compartment_2 = split_string(line)
+        char = get_char(compartment_1, compartment_2)
+        prio = calculate_priority(char)
+        sum_priority += prio
 
+    print("sum_priority:", sum_priority)
--- dec3/pt2.py
+++ dec3/pt2.py
@@ -3,33 +3,35 @@ def load_data():
         data = input_file.read().split("\n")
     return data
 
+
 data = load_data()
 
 # split input into groups of 3
 def split_input(data):
     groups = []
     for i in range(0, len(data), 3):
-        group = data[i:i+3]
+        group = data[i : i + 3]
         groups.append(group)
     return groups
 
+
 groups = split_input(data)
 
-#check if character is in group
+# check if character is in group
 def get_char(group):
     for char in group[0]:
         if char in group[1] and char in group[2]:
             return char
     return None
 
-# calculate priority
+
 def calculate_priority(char):
     if char.isupper():
         return ord(char) - 38
     else:
         return ord(char) - 96
 
-#sum up the priorities
+
 sum_priority = 0
 
 for group in groups:
@@ -38,4 +40,3 @@ for group in groups:
     sum_priority += prio
 
 print("sum priority:", sum_priority)
-
