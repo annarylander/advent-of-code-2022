59d04efa1d8a3a0540cd4282363d4ff595ea8be7
Anna Rylander <annaelida.rylander@gmail.com>
2022-12-09 19:38:45 +0100

    pt 1 and 2

---
+++ dec4/pt2.py
@@ -0,0 +1,21 @@
+#load input data
+def load_data():
+    with open("input.txt", "r") as input_file:
+        data = input_file.read().split("\n")
+    return data
+
+data = load_data()
+
+
+pair_overlaps = 0
+
+#loop through all pairs and check if they overlap
+for line in data:
+    pair = line.replace(",", "-").split("-")
+           
+    if int(pair[2]) <= int(pair[1]) and int(pair[3]) >= int(pair[0]):
+        pair_overlaps += 1
+    elif int(pair[2]) >= int(pair[0]) and int(pair[3]) <= int(pair[1]):
+        pair_overlaps += 1
+
+print("pair overlaps:", pair_overlaps)
